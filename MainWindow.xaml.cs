8922f667f6a8fe58cbcca9c76614ed52ab5b8e18
Tadj <eddinetadj@gmail.com>
2023-11-01 09:28:25 -0400

    new version version
    sssss enter the commit message for your changes. Lines starting
    new version
    version
    new version
    version
    new version
    version
    new version
    version
    new version
    version
    new version
    version
    new version
    version
    new version
    version
    new version
    version
    new version
    version

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace CampusManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<prgram> mesProg = new List<prgram>();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+
+        }
+
+
+         class prgram
+        {
+            public string nomDuProg { get; set; }
+            public string numDuprog { get; set; }
+            public string dureeDuprog { get; set; }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ProgrammesHelpers.cs
@@ -0,0 +1,27 @@
+﻿using CampusManager;
+using System.Collections.Generic;
+using System.IO;
+
+internal static class ProgrammesHelpers
+{
+
+    public static List<Progra> GetProgras()
+    {
+
+        var file = @"C:\Users\eddin\OneDrive\Desktop\Informatique,PROG-001,4 ans.txt";
+        var lines = File.ReadAllLines(file);
+        var list = new List<Progra>();
+        for (int i = 0; 1 < lines.Length; i++)
+        {
+            var line = lines[i].Split(',');
+            var progr = new Progra()
+            {
+                NomDuProg = line[0],
+                NumDuProg = line[1],
+                DureeDuProg = line[3]
+            };
+            list.Add(progr);
+        }
+        return list;
+    }
+}
